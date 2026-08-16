f274425af28589900e2f5b325b29234e40fa1b91
ArturSepp <artursepp@gmail.com>
2026-08-16 11:20:04 +0200

    Add persistent Search Console verification

--- docs/conf.py
+++ docs/conf.py
@@ -19,6 +19,7 @@ extensions = ["myst_parser"]
 myst_enable_extensions = ["colon_fence"]
 myst_heading_anchors = 3
 
+templates_path = ["_templates"]
 exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 
 html_theme = "furo"
